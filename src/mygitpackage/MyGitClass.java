90b452ffc697b8fecc09c5d0991ecf13dfaf24bf
unknown <heratan2501@gmail.com>
2021-04-03 09:54:15 -0700

    mesaj2

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -1,10 +1,12 @@
 package mygitpackage;
 
 public class MyGitClass {
-
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		System.out.println("Version 1");
+		System.out.println("java a");
+		System.out.println("java cozmek iyidir");
 
+		
 	}
 
 }
